e944c059be72dde7b41f668898ad8b702340b972
c0rdeiro <franciscordeiro99@gmail.com>
2023-08-04 21:28:16 +0100

    initial layout

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+type HeaderProps = {}
+
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <div className="justify-between items-center flex h-16 min-w-screen bg-yellow-600 px-4 py-2 ">
+      Galaxer
+    </div>
+  )
+}
+
+export default Header
